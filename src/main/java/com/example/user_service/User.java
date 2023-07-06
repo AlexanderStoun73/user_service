7c7e3d9c3137d2321b1659ec95da93e8e6a6058e
aleksandrstoun73 <aleksandrstoun73@gmail.com>
2023-07-06 19:32:53 +0600

    Initial commit

---
+++ src/main/java/com/example/user_service/User.java
@@ -0,0 +1,30 @@
+package com.example.user_service;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+        System.out.println("Пароль успешно изменен.");
+    }
+}
